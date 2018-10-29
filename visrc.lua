698f7116e52e362a999363d804453963281f64bd
Eric Mathison <dev@ericmathison.io>
2018-10-29 01:26:38 -0700

    Use two space indentation

--- visrc.lua
+++ visrc.lua
@@ -4,19 +4,20 @@ require('vis')
 require('editorconfig/editorconfig')
 
 vis.events.subscribe(vis.events.INIT, function()
-    -- Your global configuration options
-    vis:map(vis.modes.NORMAL, "\\q", function()
-        vis:command('q!')
-    end)
+  -- Your global configuration options
+  vis:map(vis.modes.NORMAL, "\\q", function()
+    vis:command('q!')
+  end)
 
-    vis:map(vis.modes.NORMAL, '<Up>', '<C-y>')
-    vis:map(vis.modes.NORMAL, '<Down>', '<C-e>')
+  vis:map(vis.modes.NORMAL, '<Up>', '<C-y>')
+  vis:map(vis.modes.NORMAL, '<Down>', '<C-e>')
 
-    vis:map(vis.modes.NORMAL, '[ ', 'O<Escape>')
-    vis:map(vis.modes.NORMAL, '] ', 'o<Escape>')
+  vis:map(vis.modes.NORMAL, '[ ', 'O<Escape>')
+  vis:map(vis.modes.NORMAL, '] ', 'o<Escape>')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-    -- Your per window configuration options e.g.
-    vis:command('set number')
+  -- Your per window configuration options e.g.
+  vis:command('set number')
 end)
+
