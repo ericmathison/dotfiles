365e257b8b957b20eeb186e4b15c39a9b6a99880
Eric Mathison <dev@ericmathison.io>
2018-11-30 20:55:25 -0800

    Use vis dark theme

--- visrc.lua
+++ visrc.lua
@@ -29,5 +29,6 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
   -- Your per window configuration options e.g.
   vis:command('set number')
   vis:command('set show-tabs')
+  vis:command('set theme dark-16')
 end)
 
